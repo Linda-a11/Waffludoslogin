1a3c268ffd1fb23208c3590d6ab62c0c6ca565ba
Linda-a11 <110931109+Linda-a11@users.noreply.github.com>
2024-09-02 15:54:16 -0500

    Add files via upload

---
+++ Conexion.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfAppLogin.View;
+
+public class Conexion
+{
+    public static SqlConnection conectarnos()
+    {
+        SqlConnection objConectar = new SqlConnection("Server=LAPTOP-O2UQOBEI\\SQLEXPRESS; Database=MVVMLoginDb;TrustServerCertificate=True; Trusted_Connection=true;");
+        objConectar.Open();
+        return objConectar;
+    }
+
+    public DataTable Indice()
+    {
+        Conectarnos();
+        DataTable dataTable = new DataTable();
+        string sql = "SELECT*FROM User";
+        SqlCommand coman = new(sql, conectarnos());
+        SqlDataAdapter adaptar = new SqlDataAdapter(coman);
+        adaptar.Fill(dataTable);
+        return dataTable;
+    }
+
+    private void Conectarnos()
+    {
+        throw new NotImplementedException();
+    }
+
+}
